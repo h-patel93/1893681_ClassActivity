fbc9cc087ef394065bc657675424cff73a679681
Harsh_Patel <harshyovaghri cchhe>
2019-06-03 14:30:16 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1893681/classactivity/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a1893681.classactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
