54e0a7f56ea1bfe193482415b3de7d5415ab1421
Harsh_Patel <harshyovaghri cchhe>
2019-06-03 15:16:25 -0400

    commit 1

--- app/src/main/java/com/example/a1893681/classactivity/MainActivity.java
+++ app/src/main/java/com/example/a1893681/classactivity/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a1893681.classactivity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn_submit,btn_exit;
+        EditText edt_email,edt_password,edt_fname,edt_lname;
+        btn_submit = findViewById(R.id.btn_submit);
+        btn_exit = findViewById(R.id.btn_exit);
+        edt_email = findViewById(R.id.edt_email);
+        edt_fname = findViewById(R.id.edt_fname);
     }
 }
